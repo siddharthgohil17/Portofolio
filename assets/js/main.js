5117bb8cdbce51718926fea48b49695484486ba7
SIDDHARTH <siddharthgohil07@gmail.com>
2023-05-24 11:02:56 +0530

    add live project link

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,16 +13,16 @@ const showMenu = (toggleId, navId) => {
 
 showMenu('nav_toggle','nav_menu')
 
-// // ACTIVE & REMOVE ACTIVE
-// const navLink = document.querySelectorAll('.nav_link')
-// navLink.forEach(n => n.classList.remove('active'))
+// ACTIVE & REMOVE ACTIVE
+const navLink = document.querySelectorAll('.nav_link')
+navLink.forEach(n => n.classList.remove('active'))
 
-// function linkAction(){
-//       navLink.forEach(n => n.classList.remove('active'))
-//       this.classList.add('active')
+function linkAction(){
+      navLink.forEach(n => n.classList.remove('active'))
+      this.classList.add('active')
 
-//       const navMenu = document.getElementById('nav_menu')
-//       navMenu.classList.remove('show')
-// }
+      const navMenu = document.getElementById('nav_menu')
+      navMenu.classList.remove('show')
+}
 
-// navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
